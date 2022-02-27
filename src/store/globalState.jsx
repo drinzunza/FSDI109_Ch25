fa34a65b2ffd93f9ccd10e16b367025e4da4a0d9
Sergio Inzunza <inxunxa@gmail.com>
2022-02-27 11:53:12 -0700

    FSDI 109 #4

--- src/store/globalState.jsx
+++ src/store/globalState.jsx
@@ -7,13 +7,37 @@ const GlobalState = (props) => {
     const [theUser, setTheUser] = useState({});
 
     const myAddToCart = (product) => {
-        let copy = [...myCart];
+      let copy = [...myCart];
+      let found = false;
+      for (let i = 0; i < copy.length; i++) {
+        let existingProd = copy[i];
+
+        if (product._id === existingProd._id) {
+          existingProd.quantity += product.quantity;
+          found = true;
+        }
+      }
+
+      if (!found) {
         copy.push(product);
-        setMyCart(copy);
+      }
+
+      setMyCart(copy);
     };
 
     const myRemoveFromCart = (productId) => {
-        console.log("Removing prod:", productId);
+      console.log("Removing prod:", productId);
+
+      let copy = [...myCart];
+      for (let i = 0; i < copy.length; i++) {
+        let prod = copy[i];
+        if (prod._id === productId) {
+          // found it, remove prod from copy
+          copy.splice(i, 1);
+        }
+      }
+
+      setMyCart(copy);
     };
 
     return (
